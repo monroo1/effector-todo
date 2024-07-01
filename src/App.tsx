2ea64a321c3deb60e683f7826888668108f87242
Andrey Roschenkov <a.roshchenkov@wis.software>
2024-07-01 14:44:34 +0300

    remove test player

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
 import Todo from "./components/todo/ui/Todo";
-import { VideoPlayer } from "./player";
 
 function App() {
-    const videoJsOptions = {
-		autoplay: true,
-		controls: true,
-		responsive: true,
-		play: ">",
-		fluid: true,
-		sources: [{
-			src: "http://saveukraine.1ce.xyz/5567/video.m3u8?token=HfLCPEomUd",
-			type: "application/x-mpegURL"
-		}]
-	}
-    return <>
-    <VideoPlayer  options={videoJsOptions}/>
-    <Todo /></>;
+    return <Todo />
 }
 
 export default App;
